d0f3595ae8c20b73c191ebc5cf5f950dc6c6ce5f
RafiaMuhammadHussain <rafiamuhammadhussain@gmail.com>
2026-05-17 22:31:11 +0500

    Done

---
+++ app.js
@@ -0,0 +1,37 @@
+function red(){
+    document.body.style.background = "#e02e2e";
+}
+function green(){
+    document.body.style.background = "#1dcb5d";
+}
+function blue(){
+    document.body.style.background = "#2e3ae0";
+}
+function purple(){
+    document.body.style.background = "#a518af";
+}
+function orange(){
+    document.body.style.background = "#d64d09";
+}
+function pink(){
+    document.body.style.background = "#d75c90";
+}
+function sky(){
+    document.body.style.background = "#2e9fe0";
+}
+function white(){
+    document.body.style.background = "#ffffff";
+    document.body.style.color = "#000000";
+}
+function darklinear(){
+    document.body.style.background = "linear-gradient(to right, black, gray, cadetblue)";
+    document.body.style.color = "#ffffff";
+}
+function lightlinear(){
+    document.body.style.background = "linear-gradient(to right, aquamarine, beige, white, burlywood)";
+    document.body.style.color = "#000000";
+}
+function resetBtn(){
+    document.body.style.background = "#000000";
+    document.body.style.color = "#ffffff";
+}
