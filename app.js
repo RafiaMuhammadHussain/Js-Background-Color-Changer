49de0ce284dcf0c5767195eee662b88a17185265
RafiaMuhammadHussain <rafiamuhammadhussain@gmail.com>
2026-05-18 16:02:36 +0500

    Done

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
-function red(){
-    document.body.style.background = "#e02e2e";
+function red() {
+    document.body.style.background = "red";
 }
-function green(){
-    document.body.style.background = "#1dcb5d";
+function green() {
+    document.body.style.background = "green";
 }
-function blue(){
-    document.body.style.background = "#2e3ae0";
+function blue() {
+    document.body.style.background = "blue";
 }
-function purple(){
-    document.body.style.background = "#a518af";
+function purple() {
+    document.body.style.background = "purple";
 }
-function orange(){
-    document.body.style.background = "#d64d09";
+function orange() {
+    document.body.style.background = "orange";
 }
-function pink(){
-    document.body.style.background = "#d75c90";
+function pink() {
+    document.body.style.background = "pink";
 }
-function sky(){
-    document.body.style.background = "#2e9fe0";
+function sky() {
+    document.body.style.background = "skyblue";
 }
-function white(){
+function white() {
     document.body.style.background = "#ffffff";
     document.body.style.color = "#000000";
 }
-function darklinear(){
+function darklinear() {
     document.body.style.background = "linear-gradient(to right, black, gray, cadetblue)";
     document.body.style.color = "#ffffff";
 }
-function lightlinear(){
+function lightlinear() {
     document.body.style.background = "linear-gradient(to right, aquamarine, beige, white, burlywood)";
     document.body.style.color = "#000000";
 }
-function resetBtn(){
+function resetBtn() {
     document.body.style.background = "#000000";
     document.body.style.color = "#ffffff";
 }
